53e50a4110a47014321786515ce9549b120bc1d4
Matt Brophy <matt@brophy.org>
2018-02-28 16:00:20 -0500

    Set up step 1 functionality

--- src/react/components/Stopwatch.jsx
+++ src/react/components/Stopwatch.jsx
@@ -2,66 +2,23 @@ import React from 'react';
 
 export class Stopwatch extends React.Component {
 
-  subscription = null;
-
   constructor() {
     super();
 
     this.state = {
       time: 0.0,
-      active: false
     };
-
-    this.start = this.start.bind(this);
-    this.stop = this.stop.bind(this);
-    this.reset = this.reset.bind(this);
   }
 
   render() {
     return (
       <div>
         <h2>{ this.toNumber(this.state.time) }</h2>
-
-        <button onClick={this.start}
-                disabled={this.state.active}>
-          Start
-        </button>
-        <button onClick={this.stop}
-                disabled={!this.state.active}>
-          Stop
-        </button>
-        <button onClick={this.reset}>
-          Reset
-        </button>
-
       </div>
     );
   }
 
   toNumber(value) {
     return value.toFixed(2);
-  }
-
-  start() {
-    if (this.state.active) return;
-
-    this.setState({ active: true })
-    this.subscription = setInterval(() =>
-      this.setState({ time: this.state.time + 0.01 }), 10);
-  }
-
-  stop() {
-    if (!this.state.active) return;
-
-    this.setState({ active: false })
-    clearInterval(this.subscription);
-    this.subscription = null;
-  }
-
-  reset() {
-    this.stop();
-    this.setState({
-      time: 0.0
-    });
   }
 }
--- src/vue/components/Stopwatch.vue
+++ src/vue/components/Stopwatch.vue
@@ -1,30 +1,14 @@
 <template>
   <div>
     <h2>{{ time | toNumber }}</h2>
-
-    <button v-on:click="start"
-            v-bind:disabled="active">
-      Start
-    </button>
-    <button v-on:click="stop"
-            v-bind:disabled="!active">
-      Stop
-    </button>
-    <button v-on:click="reset">
-      Reset
-    </button>
   </div>
 </template>
 
 <script>
 export default {
-  created() {
-      this.subscription = null;
-  },
   data() {
     return {
       time: 0.0,
-      active: false
     };
   },
   filters: {
@@ -32,25 +16,5 @@ export default {
       return value.toFixed(2);
     }
   },
-  methods: {
-    start() {
-      if (this.active) return;
-
-      this.active = true;
-      this.subscription = setInterval(() =>
-        this.time += 0.01, 10);
-    },
-    stop() {
-      if (!this.active) return;
-
-      this.active = false;
-      clearInterval(this.subscription);
-      this.subscription = null;
-    },
-    reset() {
-      this.stop();
-      this.time = 0.0;
-    }
-  }
 }
 </script>
